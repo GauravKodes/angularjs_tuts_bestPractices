a4c0cabd0a82efd2d19bcb319dd29bd39ca7d4fa
Gaurav Kundu <gaurav.jobs22@gmail.com>
2017-07-27 17:57:39 +0530

    Second Code

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,8 +2,15 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
+    stylesUrl: 'phone-list/phone-list.template.css',
+    controller: function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+        
+        $http.get('../phones/phones.json').then(function(response) {
+            self.phones = response.data;
+        });
+      /*this.phones = [
         {
           name: 'Nexus S',
           snippet: 'Fast just got faster with Nexus S.',
@@ -11,13 +18,13 @@ angular.
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
           snippet: 'The Next, Next Generation tablet.',
-          age :'3'
+          age :'2'
         }, {
           name: 'MOTOROLA XOOM™',
           snippet: 'The Next, Next Generation tablet.',
-          age : '2'
+          age : '3'
         }
       ];
-        this.orderProp="age";
+        this.orderProp="age";*/
     }
   });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -16,7 +16,7 @@ describe('PhoneCat Application', function() {
         browser.get('index.html');
     });
     
-    it('should filter the phone list as user types into the search box', function() {
+   /* it('should filter the phone list as user types into the search box', function() {
         var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
         var query = element(by.model('$ctrl.query'));
         
@@ -37,6 +37,30 @@ describe('PhoneCat Application', function() {
         query.sendkeys('apple');
         expect(phoneList.count()).toBe(0);
         
-        });
+        });*/
+    it('we should be able to control the order by drop-down menu', function() {
+       var queryField = element(by.model('$ctrl.query'));
+       var orderSelect = element(by.model('$ctrl.orderProp'));
+       var nameOption = orderSelect.element(by.css('option[value="name"]'));
+       var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+        
+        function getNames() {
+            return phoneNameColumn.map(function(elem) {
+                                       return elem.getText();
+                                       });
+        }
+        queryField.sendKeys('tablet');
+            
+        expect(getNames()).toEqual([
+            'Motorola XOOM\u2122 with Wi-Fi',
+            'MOTOROLA XOOM\u2122'
+        ]);
+        nameOption.click();
+
+        expect(getNames()).toEqual([
+            'MOTOROLA XOOM\u2122',
+            'Motorola XOOM\u2122 with Wi-Fi'
+        ]);
+    });
         
     });
